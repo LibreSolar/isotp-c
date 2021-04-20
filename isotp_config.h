d71285d085daf096318b24d565b4fb25affe8e8f
Martin Jäger <martin@libre.solar>
2021-04-20 20:16:29 +0200

    Reduce default block size for ESP32
    Block-size of 8 was leading to lost CAN frames.

--- isotp_config.h
+++ isotp_config.h
@@ -1,17 +1,20 @@
 #ifndef __ISOTP_CONFIG__
 #define __ISOTP_CONFIG__
 
-/* Max number of messages the receiver can receive at one time, this value 
+/* Max number of messages the receiver can receive at one time, this value
  * is affectied by can driver queue length
+ *
+ * The ESP32 has a 64-byte RX FIFO and each message can have a length of up
+ * to 13 bytes (including extended ID). So this value has to be set to 4.
  */
-#define ISO_TP_DEFAULT_BLOCK_SIZE   8
+#define ISO_TP_DEFAULT_BLOCK_SIZE   4
 
-/* The STmin parameter value specifies the minimum time gap allowed between 
+/* The STmin parameter value specifies the minimum time gap allowed between
  * the transmission of consecutive frame network protocol data units
  */
 #define ISO_TP_DEFAULT_ST_MIN       0
 
-/* This parameter indicate how many FC N_PDU WTs can be transmitted by the 
+/* This parameter indicate how many FC N_PDU WTs can be transmitted by the
  * receiver in a row.
  */
 #define ISO_TP_MAX_WFT_NUMBER       1
@@ -26,4 +29,3 @@
 #define ISO_TP_FRAME_PADDING
 
 #endif
-
